56f832cf9f26f201ab50705e701771a1014180b8
Charlie Broom <broomc425@gmail.com>
2020-03-05 00:56:02 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -4,3 +4,25 @@ def directors_database
 	rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
   @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
 end
+
+def directors_totals(source)
+  result = []
+  director_index = 0 
+  while director_index < source.size do
+    director = source[director_index].length 
+    result[director[:name]] = gross_for_director(director)
+    director_index += 1 
+  end
+  result
+end
+
+def gross_for_director(d)
+  totals = 0 
+  i = 0 
+  while i < d[:movies].length do
+    totals += d[:movies][index][:worldwide_gross]
+    index += 1 
+  end
+  totals
+end
+
